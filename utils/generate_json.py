e73bca2ae2e65173da2a5f0ca5ddb8ac9b4ec38e
Phantom0174 <darrenhsiou@gmail.com>
2024-10-31 11:46:44 +0800

    learn pytorch

---
+++ utils/generate_json.py
@@ -0,0 +1,20 @@
+import json
+
+root_dir = "./archive/PRINTED_TEX_230k"
+
+with open(f'{root_dir}/corresponding_png_images.txt', 'r', encoding='utf-8') as images_file:
+    images = images_file.readlines()
+
+with open(f'{root_dir}/final_png_formulas.txt', 'r', encoding='utf-8') as formulas_file:
+    formulas = formulas_file.readlines()
+
+image_formula_map = {}
+for image, formula in zip(images, formulas):
+    image = image.strip()  
+    formula = formula.strip()  
+    image_formula_map[image] = formula  
+
+with open('image_formula_mapping.json', 'w', encoding='utf-8') as json_file:
+    json.dump(image_formula_map, json_file, ensure_ascii=False, indent=4)
+
+print("JSON file created successfully!")
